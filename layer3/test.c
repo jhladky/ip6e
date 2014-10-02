816706dd4d2c5fde6791aecb191c053ac00e2383
Jacob Hladky <jacob.hladky@gmail.com>
2014-10-02 00:47:20 -0700

    Setup test driver to open tun device

--- layer3/test.c
+++ layer3/test.c
@@ -1,13 +1,71 @@
 #include <stdio.h>
-#include "l3.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+//#include "l3.h"
+#include "test.h"
 
 /*
-
-So what the testbench is going to do is open a file, full of test packets, and pass each one to the library functions, then we can test for what the results are supposed to be
-
+So let's try to make this thing open tun right now
  */
 
+static int tunFd;
+
 int main() {
+   struct ifreq ifr;
+   int err;
+
+   add_handler(SIGINT, clean_exit);
+
+   if ((tunFd = open("/dev/net/tun", O_RDWR)) == -1) {
+      perror("Failed to open tap device");
+      abort();
+   }
+
+   memset(&ifr, 0, sizeof(struct ifreq));
+   ifr.ifr_flags = IFF_TUN;
+   strncpy(ifr.ifr_name, "ip6e_test", IFNAMSIZ);
+
+   if ((err = ioctl(tunFd, TUNSETIFF, (void *) &ifr)) == -1) {
+      close(tunFd);
+      perror("Failed to configure tap device");
+      abort();
+   }
+
+   printf("Opening tap device successful!\n");
+
+   while (true) {
+
+   }
 
    return 0;
+}
+
+static void clean_exit(int unused) {
+   printf("Exiting on signal.\n");
+   close(tunFd);
+   exit(EXIT_SUCCESS);
+}
+
+static void add_handler(int signal, void (*handlerFunc)(int)) {
+   struct sigaction sa;
+
+   sa.sa_handler = handlerFunc;
+   sigemptyset(&sa.sa_mask);
+   sa.sa_flags = 0;
+   if (sigaction(signal, &sa, NULL) < 0)  {
+      perror("Adding signal handler failed");
+      abort(); //justified
+   }
 }
---
+++ layer3/test.h
@@ -0,0 +1,9 @@
+#ifndef __TEST_LAYER3__
+
+#define __TEST_LAYER3__
+
+static void clean_exit(int unused);
+static void add_handler(int signal, void (*handlerFunc)(int));
+
+
+#endif
